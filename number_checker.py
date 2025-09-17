0b116c665f1e2c3457a653e9a324a28053560a13
satya2791 <boddupallisatyavalli2791@gmail.com>
2025-09-17 14:59:05 +0530

    Add files via upload
    Added program to check even or odd number

---
+++ number_checker.py
@@ -0,0 +1,10 @@
+# Program to check if a number is positive, negative or zero
+# Taking input from the user
+users_input = int(input("Enter a number: "))
+# checking the number
+if users_input > 0:
+    print("The number is positive.")
+elif users_input < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
